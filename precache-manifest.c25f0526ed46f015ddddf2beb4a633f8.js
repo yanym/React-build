3be380ead68a05010d0ed347d3f910d065c11f18
Yiming <ymyan6@gmail.com>
2019-01-08 14:56:39 -0800

    first commit

---
+++ precache-manifest.c25f0526ed46f015ddddf2beb4a633f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ae0a7acb7bb0bd8785b",
+    "url": "/static/js/main.9ae0a7ac.chunk.js"
+  },
+  {
+    "revision": "cfde5d0c891078edd662",
+    "url": "/static/js/1.cfde5d0c.chunk.js"
+  },
+  {
+    "revision": "9ae0a7acb7bb0bd8785b",
+    "url": "/static/css/main.45543f56.chunk.css"
+  },
+  {
+    "revision": "cfde5d0c891078edd662",
+    "url": "/static/css/1.e57ea1dd.chunk.css"
+  },
+  {
+    "revision": "40d4ebf07d87c1a0fe91747a8ff9ec66",
+    "url": "/index.html"
+  }
+];
